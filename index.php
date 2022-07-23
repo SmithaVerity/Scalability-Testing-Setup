4bcbc934c2874f4bf94948ba6913159a0b256f2a
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-07-23 17:45:52 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+ <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> Scalability Testing </title>
+  </head>
+  <body>
+    <div class="main_page">
+       <center>
+           <h3>Public IP Address of user is:</h3>
+
 <?php 
 // Declaring a variable to hold the IP
 // address getHostName() gets the name 
@@ -26,3 +37,7 @@ echo " ,  ";
 //echo getIPAddress();
 echo $realIP = file_get_contents("http://ipecho.net/plain");
 ?> 
+           <p id="gfg"></p>
+       </center>
+    </div>
+</html>
