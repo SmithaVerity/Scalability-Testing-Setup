8faa1b850e717fe9a250af222329c2501b1f8741
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-07-23 17:40:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,21 @@ $localIP = getHostByName(getHostName());
   
 // Displaying the address  
 echo $localIP; 
-  
+function getIPAddress() {  
+  //whether ip is from the share internet
+  if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+      $ip = $_SERVER['HTTP_CLIENT_IP'];  
+   }  
+  //whether ip is from the proxy 
+  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+  }  
+  //whether ip is from the remote address 
+  else{  
+         $ip = $_SERVER['REMOTE_ADDR'];  
+  }  
+  return $ip;  
+}
+
+echo getIPAddress();
 ?> 
