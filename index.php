83600b7326050cb91c8482e6f60a49f2c79c8a4e
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-07-23 17:47:17 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,36 +8,7 @@
     <div class="main_page">
        <center>
            <h3>Public IP Address of user is:</h3>
-
-<?php 
-// Declaring a variable to hold the IP
-// address getHostName() gets the name 
-// of the local machine getHostByName() 
-// gets the corresponding IP 
-$localIP = getHostByName(getHostName()); 
-  
-// Displaying the address  
-echo $localIP; 
-function getIPAddress() {  
-  //whether ip is from the share internet
-  if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
-      $ip = $_SERVER['HTTP_CLIENT_IP'];  
-   }  
-  //whether ip is from the proxy 
-  elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-  }  
-  //whether ip is from the remote address 
-  else{  
-         $ip = $_SERVER['REMOTE_ADDR'];  
-  }  
-  return $ip;  
-}
-echo " ,  ";
-//echo getIPAddress();
-echo $realIP = file_get_contents("http://ipecho.net/plain");
-?> 
-           <p id="gfg"></p>
+           <p id="gfg"><?php echo $realIP = file_get_contents("http://ipecho.net/plain"); ?></p>
        </center>
     </div>
 </html>
