0362421ece3e9321ab17d5c308916e236b7aa9df
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-07-23 17:36:56 +0530

    Update scalability.sh

--- scalability.sh
+++ scalability.sh
@@ -1,2 +1,2 @@
-sudo apt update && sudo apt install apache2 -y && sudo apt install php
+sudo apt update && sudo apt install apache2 -y && sudo apt install php -y
 sudo mv test.html /var/www/html/index.html
